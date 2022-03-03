e56fe83c6c2dd0113688d09e7a21ccefcecdae7f
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-03 17:38:03 +0600

    event loop

---
+++ 38-7-js-event-loop.js
@@ -0,0 +1,7 @@
+//watch the below video.
+//what is the hack is the event loop anyway?
+
+//also find article from MDN document.
+
+//stack: sequentially working.
+//queue: synchronized way to work.
