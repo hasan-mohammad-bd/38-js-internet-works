e49c04c33e2cc41df5ecacca8f8cf373b329698a
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-03 13:16:11 +0600

    dom and render tree

---
+++ 38-2-dom,Render-trees.js
@@ -0,0 +1,14 @@
+//browser:
+//static site: only make with html and css.
+
+//browser is software: 
+//the way Html keep its element is called DOM tree. 
+
+//process how a page execute:
+//dom tree and style structure combined make Render Tree(render: showing data) => paint(then paint all data). => display the data to the user 
+
+//user interface: the displayed section to the user is called user interface.
+
+//browser has engine: which execute the image,js file, html etc.
+
+//if it is changed the any css html file. the render tree rerender and then execute the file.
