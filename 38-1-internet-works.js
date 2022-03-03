24d5ea5cbc2b4064cd43b6fd61a20fe9571f51c2
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-03 12:45:09 +0600

    first commit

---
+++ 38-1-internet-works.js
@@ -0,0 +1,24 @@
+//phone number is unique the same way the internet website is unique, this is called IP address.
+
+//you can safe the phone number with any name but still the phone number is not changing, the same way the website name can be anything but the ip address not changing.
+
+//DNS: website name converted to ip address. 
+
+//DNS loop up:  A DNS lookup, in a general sense, is the process by which a DNS record is returned from a DNS server.
+
+//tcp ip layers: find it the google. 
+
+//https: it a protocol sending the data in secure.
+
+//internet works>
+//server
+//data
+//name
+//DNS lookup 
+//ip address
+//dynamic or static data will be loaded.
+
+//find the video: how does the internet works ?
+
+
+
